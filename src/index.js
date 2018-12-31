5e68b25788390e2f340b2f0bd60c52e1209c6e8d
EduardoBeserra <beserra.eduardo@gmail.com>
2018-12-31 09:26:46 -0200

    Pequena alteracao

--- src/index.js
+++ src/index.js
@@ -9,9 +9,14 @@ const URL = 'http://10.150.61.20:8080/IntegradorProgress/rest/restful/cademp'
     + '?where=cademp.empresa = 1 and cademp.unidade > 0&campos=cademp.unidade,cademp.sigla'
     + '&configBD=BDDesenv.properties'
 
+const lista = [
+    {id: "teste1", descricao: "teste1"},
+    {id: "teste2", descricao: "teste2"},
+]
 ReactDOM.render(
     <div className="container">
         <ChooseSimples url={URL} id='cademp_unidade' descricao='cademp_sigla'/>
+        <ChooseSimples lista={lista} id='id' descricao='descricao'/>
     </div>,
     document.getElementById('root')
 );
